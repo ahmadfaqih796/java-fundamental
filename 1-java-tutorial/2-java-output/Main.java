c5c2f009a2ec6f666111298df05cc3b570754722
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-26 23:53:22 +0700

    java output

---
+++ 1-java-tutorial/2-java-output/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+      System.out.println("I am learning Java.");
+      System.out.println("It is awesome!");
+   }
+}
