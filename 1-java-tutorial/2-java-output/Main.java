6ba2d64aea704968353de430fe7154857f597bf8
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-07-27 00:31:05 +0700

    print text and numbers

--- 1-java-tutorial/2-java-output/Main.java
+++ 1-java-tutorial/2-java-output/Main.java
@@ -1,7 +1,12 @@
 public class Main {
    public static void main(String[] args) {
+      // Print Text
       System.out.println("Hello World!");
       System.out.println("I am learning Java.");
       System.out.println("It is awesome!");
+      // Print Numbers
+      System.out.println(3);
+      System.out.println(358);
+      System.out.println(50000);
    }
 }
