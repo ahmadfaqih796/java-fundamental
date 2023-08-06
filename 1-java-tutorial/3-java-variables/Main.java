d085ed943f64c0a63caa8e8cf42c91e32f34ae9d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-06 22:16:14 +0700

    Identifiers

--- 1-java-tutorial/3-java-variables/Main.java
+++ 1-java-tutorial/3-java-variables/Main.java
@@ -4,14 +4,19 @@ public class Main {
       int myNum;
       myNum = 15;
       System.out.println(myNum);
+
       // Print Variable
       String firstName = "John ";
       String lastName = "Doe";
       String fullName = firstName + lastName;
       System.out.println(fullName);
 
-      // Declare Mulitiple
+      // Declare Mulitiple Variables
       int x = 5, y = 6, z = 50;
       System.out.println(x + y + z);
+
+      // Identifiers
+      // int minutesPerHour = 60; // Good
+      // int m = 60; // OK, but not so easy to understand what m actually is
    }
 }
