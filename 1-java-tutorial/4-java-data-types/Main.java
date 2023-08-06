f976e72f099f80f9d8d56a3c9aabf50a9ca2d94b
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-06 22:21:28 +0700

    type data

---
+++ 1-java-tutorial/4-java-data-types/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+   public static void main(String[] args) {
+      int myNum = 5; // Integer (whole number)
+      float myFloatNum = 5.99f; // Floating point number
+      char myLetter = 'D'; // Character
+      boolean myBool = true; // Boolean
+      String myText = "Hello"; // String
+   }
+}
