32ea3e67c11dc4eb828b8dc5eaef6b6d4a7d799d
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-06 22:09:19 +0700

    print variable

--- 1-java-tutorial/3-java-variables/Main.java
+++ 1-java-tutorial/3-java-variables/Main.java
@@ -4,5 +4,10 @@ public class Main {
       int myNum;
       myNum = 15;
       System.out.println(myNum);
+      // Print Variable
+      String firstName = "John ";
+      String lastName = "Doe";
+      String fullName = firstName + lastName;
+      System.out.println(fullName);
    }
 }
