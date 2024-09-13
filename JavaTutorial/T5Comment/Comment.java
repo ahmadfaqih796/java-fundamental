ce8f5b71af1d4406ac63954566886bdb2f819e2e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-09-13 11:28:03 +0700

    comment

---
+++ JavaTutorial/T5Comment/Comment.java
@@ -0,0 +1,13 @@
+package JavaTutorial.T5Comment;
+
+public class Comment {
+
+   public void printMessage() {
+      // This is a comment
+      System.out.println("Hello World");
+      /*
+       * The code below will print the words Hello World to the screen, and it is
+       * amazing
+       */
+   }
+}
