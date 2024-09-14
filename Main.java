0b94d870b751a41637b683a5f72b410d1614570e
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-09-14 23:57:49 +0700

    entity

--- Main.java
+++ Main.java
@@ -3,13 +3,23 @@ import Java.J2Classes.C1Class.Classes;
 public class Main {
    public static void main(String[] args) {
       Main coba = new Main();
-      coba.coba("Faqih");
+      coba.coba("Faqih 1");
+
+      CobaEntity vvv = new CobaEntity(1L, "Faqih", 20);
+      System.out.println(vvv.getAge());
 
       Classes cccc = new Classes();
       cccc.printMessage();
+      System.out.println("xxxxxxxxxxx");
    }
 
    public void coba(String nama) {
       System.out.println("nama saya " + nama);
+   }
+
+   public void xxx() {
+      String nama = "Faqih";
+      System.out.println(nama);
+      return;
    }
 }
